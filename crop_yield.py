c28e300ea4f7448a44cb87b4c12065f0d3d72aca
Jerome Siljan <Jerome.Siljan@mavs.uta.edu>
2023-01-06 19:41:57 +0530

    feat(crop_yield.py): calculates r^2

--- crop_yield.py
+++ crop_yield.py
@@ -2,16 +2,16 @@
 crop_yield predicts crop yield using linear regression
 """
 
+import matplotlib.pyplot as plt
 import pandas as pd
+import seaborn as sns
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+from sklearn.model_selection import train_test_split
 
 df = pd.read_csv("FAOSTAT_data_en_1-6-2023.csv")
 
-#               df.describe(include="all")
-# shows that "Domain Code", "Domain", "Area Code (M49)", "Area", "Item", "Item Code (CPC)" have the same value for all
-# "Flag", "Flag Description" have 2 different values, useless to me
-# Picking "Year" instead of "Year Code"
-# "Element Code" seems useless
-
+# remove unnecessary columns
 df.drop(
     columns=[
         "Area Code (M49)",
@@ -28,4 +28,39 @@ df.drop(
     inplace=True,
 )
 
-print(df)
+# shows values over time in line plot
+# sns.lineplot(x="Year", y="Value", data=df, hue="Element")
+
+# pivot values
+df_pivot = (
+    df.pivot(index="Year", columns="Element", values="Value")
+    .reset_index()
+    .drop(columns=["Production"])
+    .rename(
+        columns={
+            "Area harvested": "Area Harvested (ha)",
+            "Yield": "Yield (hg/ha)",
+        }
+    )
+)
+
+# setting X, Y, training and testing sets
+X = df_pivot.drop(columns=["Yield (hg/ha)"])
+Y = df_pivot["Yield (hg/ha)"]
+
+X_train, X_test, Y_train, Y_test = train_test_split(
+    X, Y, test_size=0.25, random_state=30
+)
+
+# generate and fit model
+lin_model = LinearRegression()
+lin_model.fit(X_train, Y_train)
+
+# show prediction vs actual data
+sns.lineplot(x="Year", y="Yield (hg/ha)", data=df_pivot)
+sns.lineplot(x="Year", y=lin_model.predict(X_test), data=X_test, color="orange")
+plt.show()
+
+# calculate r2 score
+r2 = r2_score(Y_test, lin_model.predict(X_test))
+print(f"R squared score: {r2:.3f}")
